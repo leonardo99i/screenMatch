b808f2e205022e2477a13d8157113be725c0c6a3
Leonardo R. Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-07-27 15:35:10 -0300

    Ajustando o que é publico e o que é privado

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -31,7 +31,7 @@ public class Filme {
         totalAvaliacao++;
     }
 
-    double mediaAvaliacoes(){
+    public double mediaAvaliacoes(){
         return somaAvaliacao / totalAvaliacao;
     }
 }
